2e7bbc9cf5e6fedcf8d19a7c2d346cbdd26faac8
mikekang47 <gyeong0308@gmail.com>
2021-02-23 16:47:44 +0900

    Sovle selfNumber task in Baekjoon 4673

---
+++ arithmeticSequence/arithmeticSequence/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  arithmeticSequence
+//
+//  Created by 강경록 on 2021/02/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
