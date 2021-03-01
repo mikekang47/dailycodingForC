8da21cfdbb9aa56af03d146bc41b0a775ab31923
mikekang47 <gyeong0308@gmail.com>
2021-03-01 23:48:51 +0900

    Sove 1157 by using C about checking alphabet number of occurance

--- wordStudy/wordStudy/main.c
+++ wordStudy/wordStudy/main.c
@@ -1,14 +1,28 @@
-//
-//  main.c
-//  wordStudy
-//
-//  Created by 강경록 on 2021/03/01.
-//
-
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main() {
+    
+    int cnt = 0, mcnt = 0;
+    char string[1000000];
+    char ma;
+    scanf("%s", string);
+    for (int i = 'a'; i <= 'z'; i++) {
+        for (int j = 0; j < strlen(string); j++) {
+            if (string[j] == i || string[j] == i - 32)
+                cnt++;
+        }
+        
+        if (mcnt < cnt) {
+            mcnt = cnt;
+            ma = i;
+            if (ma >= 97)
+                ma -= 32;
+        }
+        else if (mcnt == cnt)
+            ma = '?';
+        
+        cnt = 0;
+    }
+    printf("%c", ma);
 }
