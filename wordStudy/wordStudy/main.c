6a17cf9cad3517fd1b100c5fb156d64f3ab45cd0
mikekang47 <gyeong0308@gmail.com>
2021-03-01 23:18:03 +0900

    Setting environment

---
+++ wordStudy/wordStudy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  wordStudy
+//
+//  Created by 강경록 on 2021/03/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
