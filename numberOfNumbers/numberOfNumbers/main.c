5e234a17e61f4bbbf7477739f9c5d6301525b42a
mikekang47 <gyeong0308@gmail.com>
2021-03-07 23:41:24 +0900

    Solved a task 2577 about number of numbers by using C

--- numberOfNumbers/numberOfNumbers/main.c
+++ numberOfNumbers/numberOfNumbers/main.c
@@ -1,14 +1,22 @@
-//
-//  main.c
-//  numberOfNumbers
-//
-//  Created by 강경록 on 2021/03/05.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    int a, b, c;
+    int array[10] = {0, };
+    int rest;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    int result = a * b * c;
+    
+    while(result > 0) {
+        rest = result % 10;
+        result = result / 10;
+        array[rest]++;
+    }
+    
+    for(int i = 0; i < 10; i++) {
+        printf("%d\n", array[i]);
+    }
     return 0;
 }
