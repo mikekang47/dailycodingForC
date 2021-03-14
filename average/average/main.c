3ada09988909adcd1ac781f7f15f1c3ed54455f8
mikekang47 <gyeong0308@gmail.com>
2021-03-14 23:03:06 +0900

    Solved a task about getting average by using C

--- average/average/main.c
+++ average/average/main.c
@@ -1,46 +1,25 @@
 #include <stdio.h>
 
-int findMax(int array[], int max, int length) {
-    for(int i = 1; i < length; i++) {
-        if(max < array[i]) {
-            max = array[i];
-        }
-    }
-    return max;
-}
-
-void modifyValue(int array[], int modified) {
-    int i = 0;
-    while(array[i] != '\0') {
-        array[i] = array[i] / modified;
-        i++;
-    }
-}
-
-double printAverage(int array[]) {
-    int sum = 0;
-    int i = 0;
-    double average = 0;
-    while(array[i] != '\0') {
-        sum += array[i];
-        i++;
-    }
-    average = sum / i;
-    return average;
-}
-
 int main() {
     int length;
-    int modified;
-    int result[1001];
+    double subject[1000];
     scanf("%d", &length);
+    
     for(int i = 0; i < length; i++) {
-        scanf("%d", &result[i]);
+        scanf("%lf", &subject[i]);
     }
-    int max = result[0];
-    modified = findMax(result, max, length) * 100;
-    modifyValue(result, modified);
-    printf("%lf\n", printAverage(result));
-    return 0;
+    
+    int max = subject[0];
+    for(int i = 0; i < length; i++) {
+        if(max < subject[i]) {
+            max = subject[i];
+        }
+    }
+    
+    double sum = 0;
+    for(int i = 0; i < length; i++) {
+        subject[i] = subject[i]/max * 100;
+        sum += subject[i];
+    }
+    printf("%.2lf\n", sum / length);
 }
-
